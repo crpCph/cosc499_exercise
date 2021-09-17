40bfbdb3ff68828647c41eb94e132bfd69986949
Tim <mammadov_teymur@yahoo.com>
2021-09-16 23:49:56 -0700

    Added sortable string array

--- sortArray/src/Main.java
+++ sortArray/src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args){
+        SortStringArray sta = new SortStringArray(40);
+
+        System.out.println("Before sort:");
+        sta.print();
+
+        System.out.println("After sort:");
+        sta.sort();
+        sta.print();
+    }
 }
