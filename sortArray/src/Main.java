07da104d2f2f6b09e810fa431409ebf560804c1a
Tim <mammadov_teymur@yahoo.com>
2021-09-17 01:22:21 -0700

    Fixing main

--- sortArray/src/Main.java
+++ sortArray/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
+
+        //Int Array
         SortIntArray sia = new SortIntArray(20);
 
         System.out.println("Before sort:");
@@ -8,5 +10,17 @@ public class Main {
         System.out.println("After sort:");
         sia.sort();
         sia.print();
+        System.out.println();
+
+        //String Array
+        SortStringArray sta = new SortStringArray(40);
+
+        System.out.println("Before sort:");
+        sta.print();
+
+        System.out.println("After sort:");
+        sta.sort();
+        sta.print();
+
     }
 }
