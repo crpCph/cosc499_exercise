af2d89f5055ffe6c27d6f0cce8ed1fa9728e73bf
Tim <mammadov_teymur@yahoo.com>
2021-09-17 00:45:16 -0700

    Added JUnit tests

---
+++ sortArray/test/SortStringArrayTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortIntArrayTest {
+
+    @Test
+    void testGenerate1() {
+        SortStringArray sta = new SortStringArray(20);
+        SortStringArray sta2 = new SortStringArray(20);
+        assertNotNull(sta.get(19));
+    }
+
+    @Test
+    void testGenerate2() {
+        SortStringArray sta = new SortStringArray(100);
+        SortStringArray sta2 = new SortStringArray(100);
+        assertNotNull(sta.get(99));
+    }
+
+    @Test
+    void testGenerate3() {
+        SortStringArray sta = new SortStringArray(300);
+        SortStringArray sta2 = new SortStringArray(300);
+        assertNotNull(sta.get(299));
+    }
+
+
+    @Test
+    void testSort() {
+        SortStringArray sta =  new SortStringArray(100);
+
+        sta.sort();
+
+        assertTrue(sta.get(0).compareTo(sta.get(1)) < 0);
+        assertTrue(sta.get(98).compareTo(sta.get(99)) < 0);
+    }
+
+}
